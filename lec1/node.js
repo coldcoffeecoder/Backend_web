1764c51a47b1bf7802bbb28718c655f6acd321d6
Bhumika <bhumikachoudhary1534@gmail.com>
2025-08-28 02:54:38 +0530

    what is nodejs

---
+++ lec1/node.js
@@ -0,0 +1,13 @@
+//what is nodejs
+//----> chrome engine v8 embedded with c++ => nodejs
+
+//Benifits:
+//-> u can run js outside of the browser
+//-> js can talk to native machine because of cpp
+//-> u can create webservers in js lang
+
+//Nodejs is neither a framework nor a libraby
+
+//nodejs is a runtime enviornment
+
+//nodejs is an open-source, cross platform js runtime enviornment
