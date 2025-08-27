de53730706fb2aa4dc4e082ebc6e1e4f86a6069d
Bhumika <bhumikachoudhary1534@gmail.com>
2025-08-28 04:53:00 +0530

    some starting points about nodejs

---
+++ lec3/node.js
@@ -0,0 +1,11 @@
+//--> chrome's v8 engine was not directly embedded in cpp{some changes}
+
+//->DOM or windom related elements were removed{also something like to them}
+
+//we knew that nodejs -> we will only run at server side
+//-> so DOM manipulation or UI related things in JS were remvoed in this
+
+//-> npm init-> initialization ->> makes template/file that help us to run our project
+//->after this a new file will be created named "package.json"-> kind of configuration file
+//in this package.json we can create our own scripts that can direcly do tasks and act as a command
+
