4fe21668887010dc6156f5356f279be91be91b0e
Bhumika <bhumikachoudhary1534@gmail.com>
2025-08-30 05:01:03 +0530

    Update module.js

---
+++ lec4/module.js
@@ -0,0 +1,19 @@
+// const math = require ("./math");
+
+// console.log("math value is ", math(2,4));
+
+const {add, sub} = require("./math");
+
+console.log("math value is", sub(2,4) );
+console.log("math value is", add(2,4));
+
+//or you can write like this
+//const math = require("./math");
+//console.log("math value is", math.add(2,4));
+//same goes for subtract
+
+//require fun is used to import
+
+
+
+
